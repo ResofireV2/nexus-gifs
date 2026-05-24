d0c90d8fa2708c5ae77c3947e823d6a2c7420425
ResofireV2 <stryguardian@gmail.com>
2026-05-23 21:21:56 -0500

    Delete backend directory

--- backend/config/prod.exs
+++
@@ -1,7 +0,0 @@
-import Config
-
-config :nexus_gifs, NexusGifsWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true
-
-config :logger, level: :info
--- backend/lib/nexus_gifs/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule NexusGifs.Repo do
-  use Ecto.Repo,
-    otp_app: :nexus_gifs,
-    adapter: Ecto.Adapters.Postgres
-end
